da19183e575cbb60034aef16de7b833fb74f1e18
arturombc <arturomagno1@gmail.com>
2025-06-05 20:45:25 -0500

    client_server_test

--- test_tcp/client/client.go
+++ test_tcp/client/client.go
@@ -1,22 +1,111 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
-	"time"
 )
+
+type Tx struct {
+	Version   uint32  `json:"version"`
+	TxIns     []TxIn  `json:"txins"`
+	TxOuts    []TxOut `json:"txouts"`
+	Locktimei uint32  `json:"locktime"`
+}
+
+type TxIn struct {
+	PrevTx    []byte   `json:"prevtx"`
+	PrevIndex uint32   `json:"previndex"`
+	Sequence  uint32   `json:"sequence"`
+	Witness   [][]byte `json:"witness"`
+	Net       string   `json:"net"`
+}
+
+type TxOut struct {
+	Amount  uint64 `json:"amount"`
+	Address string `json:"address"`
+}
+
+// NewTx creates a new transaction
+func NewTx(version uint32, locktime uint32) *Tx {
+	return &Tx{
+		Version:   version,
+		TxIns:     make([]TxIn, 0),
+		TxOuts:    make([]TxOut, 0),
+		Locktimei: locktime,
+	}
+}
+
+// AddInput adds an input to the transaction
+func (tx *Tx) AddInput(prevTx []byte, prevIndex uint32, sequence uint32, net string) {
+	txin := TxIn{
+		PrevTx:    prevTx,
+		PrevIndex: prevIndex,
+		Sequence:  sequence,
+		Witness:   make([][]byte, 0),
+		Net:       net,
+	}
+	tx.TxIns = append(tx.TxIns, txin)
+}
+
+// AddOutput adds an output to the transaction
+func (tx *Tx) AddOutput(amount uint64, address string) {
+	txout := TxOut{
+		Amount:  amount,
+		Address: address,
+	}
+	tx.TxOuts = append(tx.TxOuts, txout)
+}
+
+// ToJSON converts the transaction to JSON
+func (tx *Tx) ToJSON() (string, error) {
+	jsonData, err := json.Marshal(tx)
+	if err != nil {
+		return "", err
+	}
+	return string(jsonData), nil
+}
+
+// CreateSampleTransaction creates a sample transaction for testing
+func CreateSampleTransaction() *Tx {
+	tx := NewTx(1, 0)
+
+	// Add sample input (spending a previous transaction)
+	prevTxHash := []byte("abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890")
+	tx.AddInput(prevTxHash, 0, 0xffffffff, "main")
+
+	// Add sample outputs
+	tx.AddOutput(100000, "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa") // 100,000 satoshis to Alice
+	tx.AddOutput(50000, "1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2")  // 50,000 satoshis to Bob
+	tx.AddOutput(25000, "1C4bc762dd5423e332166702cb75f40df79") // 25,000 satoshis change
+
+	return tx
+}
 
 func main() {
 	// Connect to the server
-	conn, err := net.Dial("tcp", "localhost:8080")
+	conn, err := net.Dial("tcp", "192.168.35.225:8080")
 	if err != nil {
 		fmt.Println("Error connecting to server:", err)
 		return
 	}
 	defer conn.Close()
 
-	// Create a message with timestamp
-	message := fmt.Sprintf("Hello from client at %s", time.Now().Format("15:04:05"))
+	// Create a sample transaction
+	tx := CreateSampleTransaction()
+
+	// Convert transaction to JSON
+	txJSON, err := tx.ToJSON()
+	if err != nil {
+		fmt.Println("Error converting transaction to JSON:", err)
+		return
+	}
+
+	// Create message with transaction
+	message := fmt.Sprintf("TRANSACTION:%s", txJSON)
+
+	fmt.Printf("Sending transaction to server...\n")
+	fmt.Printf("Transaction JSON: %s\n", txJSON)
 
 	// Send message to the server
 	_, err = conn.Write([]byte(message + "\n"))
@@ -33,6 +122,5 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Sent: %s\n", message)
 	fmt.Printf("Server response: %s", string(response[:n]))
 }
--- test_tcp/server/server.go
+++ test_tcp/server/server.go
@@ -2,11 +2,85 @@ package main
 
 import (
 	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"net"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
+
+type Tx struct {
+	Version   uint32  `json:"version"`
+	TxIns     []TxIn  `json:"txins"`
+	TxOuts    []TxOut `json:"txouts"`
+	Locktimei uint32  `json:"locktime"`
+}
+
+type TxIn struct {
+	PrevTx    []byte   `json:"prevtx"`
+	PrevIndex uint32   `json:"previndex"`
+	Sequence  uint32   `json:"sequence"`
+	Witness   [][]byte `json:"witness"`
+	Net       string   `json:"net"`
+}
+
+type TxOut struct {
+	Amount  uint64 `json:"amount"`
+	Address string `json:"address"`
+}
+
+// GenerateTxID generates a unique transaction ID
+func (tx *Tx) GenerateTxID() string {
+	jsonData, _ := json.Marshal(tx)
+	hash := sha256.Sum256(jsonData)
+	return hex.EncodeToString(hash[:])[:16] // Use first 16 chars as ID
+}
+
+// SaveTransaction saves a transaction to disk
+func SaveTransaction(tx *Tx) error {
+	// Create transactions directory if it doesn't exist
+	err := os.MkdirAll("transactions", 0755)
+	if err != nil {
+		return fmt.Errorf("failed to create transactions directory: %v", err)
+	}
+
+	// Generate transaction ID
+	txID := tx.GenerateTxID()
+
+	// Create filename with timestamp and transaction ID
+	timestamp := time.Now().Format("2006-01-02_15-04-05")
+	filename := fmt.Sprintf("tx_%s_%s.json", timestamp, txID)
+	filepath := filepath.Join("transactions", filename)
+
+	// Convert transaction to pretty JSON
+	jsonData, err := json.MarshalIndent(tx, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal transaction: %v", err)
+	}
+
+	// Write to file
+	err = os.WriteFile(filepath, jsonData, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write transaction file: %v", err)
+	}
+
+	fmt.Printf("Transaction saved: %s (ID: %s)\n", filename, txID)
+	return nil
+}
+
+// ParseTransaction parses a transaction from JSON string
+func ParseTransaction(jsonStr string) (*Tx, error) {
+	var tx Tx
+	err := json.Unmarshal([]byte(jsonStr), &tx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse transaction JSON: %v", err)
+	}
+	return &tx, nil
+}
 
 func main() {
 	// Listen on port 8080
@@ -17,7 +91,8 @@ func main() {
 	}
 	defer listener.Close()
 
-	fmt.Println("Server listening on port 8080...")
+	fmt.Println("Transaction Server listening on port 8080...")
+	fmt.Println("Ready to receive and save transactions...")
 
 	for {
 		// Accept incoming connections
@@ -48,12 +123,48 @@ func handleClient(conn net.Conn) {
 		}
 
 		messageCount++
-		fmt.Printf("Received from %s: %s\n", clientAddr, message)
+		fmt.Printf("Received from %s: message #%d\n", clientAddr, messageCount)
+
+		var response string
+
+		// Check if message is a transaction
+		if strings.HasPrefix(message, "TRANSACTION:") {
+			// Extract JSON part
+			jsonPart := strings.TrimPrefix(message, "TRANSACTION:")
+
+			// Parse transaction
+			tx, err := ParseTransaction(jsonPart)
+			if err != nil {
+				response = fmt.Sprintf("ERROR: Failed to parse transaction: %v\n", err)
+				fmt.Printf("Error parsing transaction from %s: %v\n", clientAddr, err)
+			} else {
+				// Save transaction
+				err = SaveTransaction(tx)
+				if err != nil {
+					response = fmt.Sprintf("ERROR: Failed to save transaction: %v\n", err)
+					fmt.Printf("Error saving transaction from %s: %v\n", clientAddr, err)
+				} else {
+					txID := tx.GenerateTxID()
+					response = fmt.Sprintf("SUCCESS: Transaction saved with ID: %s\n", txID)
+					fmt.Printf("Transaction successfully saved from %s (ID: %s)\n", clientAddr, txID)
+
+					// Print transaction details
+					fmt.Printf("  Version: %d, Inputs: %d, Outputs: %d, Locktime: %d\n",
+						tx.Version, len(tx.TxIns), len(tx.TxOuts), tx.Locktimei)
+
+					// Print output details
+					for i, out := range tx.TxOuts {
+						fmt.Printf("  Output %d: %d satoshis to %s\n", i, out.Amount, out.Address)
+					}
+				}
+			}
+		} else {
+			// Regular message
+			response = fmt.Sprintf("Server received message #%d at %s\n",
+				messageCount, time.Now().Format("15:04:05"))
+		}
 
 		// Send response back to client
-		response := fmt.Sprintf("Server received message #%d at %s\n",
-			messageCount, time.Now().Format("15:04:05"))
-
 		_, err := conn.Write([]byte(response))
 		if err != nil {
 			fmt.Printf("Error sending response to %s: %v\n", clientAddr, err)
